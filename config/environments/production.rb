da7ac372d902331445279d27a678aa1250ea5a60
Rus <ad@LENOVO.(none)>
2012-06-25 14:37:32 +0300

    updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ SampleApp::Application.configure do
   config.assets.precompile += %w( *.css *.js) 
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
