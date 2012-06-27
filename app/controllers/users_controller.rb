b365ad7428b363a750cf9a79b45c21a182671666
Rus <ad@LENOVO.(none)>
2012-06-27 16:23:58 +0300

    DOne with sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,10 +14,11 @@ class UsersController < ApplicationController
 def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Добро пожаловать!"
       redirect_to @user
     else
-      @title = "Sign up"
+      @title = "Зарегистрироваться"
       render 'new'
     end
   end
