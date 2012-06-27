af0050150387b6d4e0abf954ee234c9831bc4cba
Rus <ad@LENOVO.(none)>
2012-06-27 17:30:33 +0300

    upds

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,8 @@
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SampleApp::Application.initialize!
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
+
