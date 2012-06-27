cf615e69a71c28f44c806b49a23c703964b880eb
Rus <ad@LENOVO.(none)>
2012-06-27 18:43:39 +0300

    went to ruby19

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-    @title = "Sign up"
+    @title = "Зарегистрироваться"
    end
 
 
@@ -17,10 +17,10 @@ def create
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome!"
+      flash[:success] = "Добро пожаловать!"
       redirect_to @user
     else
-      @title = "Sign up"
+      @title = "Зарегистрироваться"
       render 'new'
     end
   end
